db8a66e390e5de995fcb8a02c90a8abee9e15de1
stretinsky <90929574+stretinsky@users.noreply.github.com>
2021-10-15 23:56:52 +0300

    Add files via upload

---
+++ ClassWork_09.10.21/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ClassWork_09._10._21
+{
+    class Program
+    {
+        static void Compare(byte[] clanOne, byte[] clanTwo)
+        {
+            if (clanOne.Count(n => n == 5) == clanTwo.Count(n => n == 5))
+            {
+                Console.WriteLine("Drinks All Around! Free Beers on Bjrong!");
+            }
+            else
+            {
+                Console.WriteLine("Ой, Бьорг - пончик! Ни для кого пива!");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            short maxValue = 15;
+            int bbbCount = r.Next(1, maxValue), ssCount = r.Next(1, maxValue);
+            byte[] bbb = new byte[bbbCount], ss = new byte[ssCount];
+            for (int i = 0; i < bbb.Length; i++)
+            {
+                bbb[i] = (byte)r.Next(10);
+            }
+            for (int i = 0; i < ss.Length; i++)
+            {
+                ss[i] = (byte)r.Next(10);
+            }
+            Compare(ss, bbb);
+
+            byte n = 1;
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "pics");
+            List<string> pics = new List<string>();
+            IEnumerable<string> pictures = Directory.EnumerateFiles(path);
+            foreach (var picture in pictures)
+            {
+                pics.Add(picture);
+                Console.WriteLine("{0}: {1}", n, picture);
+                n++;
+            }
+            for (int i = pics.Count() - 1; i >= 1; i--)
+            {
+                int j = r.Next(i + 1);
+                string temp = pics[j];
+                pics[j] = pics[i];
+                pics[i] = temp;
+            }
+            Console.WriteLine();
+            n = 1;
+            foreach (var pic in pics)
+            {
+                Console.WriteLine("{0}: {1}", n, pic);
+                n++;
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ HomeWork_09.10.21/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_09._10._21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Task 2. Введите речь Гордона");
+            string input = Console.ReadLine().ToUpper();
+            input = input.Replace('А', '@');
+            input = input.Replace('A', '@');
+            string vovels = "EYUOЕЫОЭЯИЮЁУI";
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (vovels.Contains(input[i]))
+                {
+                    input = input.Replace(input[i], '*');
+                }
+            }
+            Console.WriteLine(input + "!!!");
+            Console.ReadKey();
+        }
+    }
+}
